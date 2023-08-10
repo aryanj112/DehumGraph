f7ec173f8085abe61ec7fd8bad6c1e5af942702a
aryanj112 <2006aryanj@gmail.com>
2023-08-10 08:43:52 -0400

    Created Mul_test.py
    Mul_test - Figure out how to visualize 4 different graphs on the same plot

--- src/combine/ambient_CH1.py
+++ src/combine/ambient_CH1.py
@@ -4,7 +4,11 @@ import pandas as pd
 import numpy as np
 
 # List of CSV file paths
-csv_files = ["data/ambient_weather/2021CH1A_use.csv", "data/ambient_weather/2022CH1A_use.csv", "data/ambient_weather/2023CH1A_use.csv"]
+csv_files = [r"C:\Users\ajayj\DehumGraph\data\ambient_weather\2019CH1A_use.CSV",    
+             r"C:\Users\ajayj\DehumGraph\data\ambient_weather\2020CH1A_use.CSV",
+             r"C:\Users\ajayj\DehumGraph\data\ambient_weather\2021CH1A_use.csv", 
+             r"C:\Users\ajayj\DehumGraph\data\ambient_weather\2022CH1A_use.csv", 
+             r"C:\Users\ajayj\DehumGraph\data\ambient_weather\2023CH1A_use.csv"]
 
 # Load and concatenate multiple CSV files
 data_frames = [pd.read_csv(file) for file in csv_files]
@@ -14,7 +18,6 @@ ambient_combine_data = pd.concat(data_frames, ignore_index=True)
 ambient_combine_data.replace("--",np.nan,inplace=True) # Replaces -- with nan values
 ambient_combine_data['Humidity(%)'] = pd.to_numeric(ambient_combine_data['Humidity(%)'], errors='coerce') # Convert humidity column to numeric values
 ambient_combine_data.dropna(inplace=True) # Drops Nan values
-#ambient_combine_data["Time"] = pd.to_datetime(ambient_combine_data["Time"]) # Fixes dates to datetime
 
 # Save the combined data to a new CSV file
 ambient_combine_data.to_csv('ambient_combine_data.csv', index=False)
---
+++ tests/ran_plot/mul_test.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+# Plot 1
+plt.subplot(2, 2, 1)
+# Your code for plot 1
+
+# Plot 2
+plt.subplot(2, 2, 2)
+# Your code for plot 2
+
+# Plot 3
+plt.subplot(2, 2, 3)
+# Your code for plot 3
+
+# Plot 4
+plt.subplot(2, 2, 4)
+# Your code for plot 4
+
+# Display all plots together
+plt.tight_layout()
+plt.show()
