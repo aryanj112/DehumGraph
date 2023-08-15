df56e5a07d8eceb1b5a99da253dca59713e17766
aryanj112 <2006aryanj@gmail.com>
2023-08-15 15:11:19 -0400

    Fixed hum_day.py
    Made it so I can plot it day by day and have a marker to see when the dehum bucket was emptied

--- src/plot/ambient/hum_day.py
+++ src/plot/ambient/hum_day.py
@@ -3,7 +3,7 @@ import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 
-# Read data and perform preprocessing
+# Read data
 ambient = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Base(guest)\CH7A_combine.CSV")
 
 # Convert Time column to datetime
@@ -14,36 +14,29 @@ ambient.replace("--", np.nan, inplace=True)  # Replaces '--' with NaN values
 ambient['Humidity(%)'] = pd.to_numeric(ambient['Humidity(%)'], errors='coerce')  # Convert humidity column to numeric values
 ambient.dropna(inplace=True)  # Drops NaN values
 
-# Calculate daily average temperature
-#ambient["Date"] = ambient["Time"].dt.date
-#daily_avg_temp = ambient.groupby("Date")["Temperature(F)"].mean()
-
-# Filter data to start from 2023-06-18
-start_date = pd.to_datetime("2023-07-04")  # Convert to datetime.date
-end_date = pd.to_datetime("2023-07-05")  # Convert to datetime.date
-#daily_avg_temp = daily_avg_temp.loc[start_date:end_date]
+# Fitlter time 
+start_date = pd.to_datetime("2023-07-04")  
+end_date = pd.to_datetime("2023-07-05")  
 ambient = ambient[ambient["Time"].between(start_date, end_date)]
 
 # Create a figure and plot
 fig, ax1 = plt.subplots()
 
-#ax1.plot(daily_avg_temp.index, daily_avg_temp.values, '-', label="Avg Temp", color="tab:red")
 ax1.plot(ambient["Time"], ambient["Humidity(%)"], '-', label="Humidity(%)", color="tab:purple")
+
+vertical_line_time = pd.to_datetime("2023-07-04 12:00") # Add vertical line at 11 o'clock on July 5th, 2023
+ax1.axvline(x=vertical_line_time, color='red', linestyle='--')
 
 ax1.set_xlabel("Date and Time")
 ax1.set_ylabel("Humidity(%)", color="tab:purple")
 
-# Set the x-axis to show hours
 ax1.xaxis.set_major_locator(mdates.HourLocator(interval=2))  # Show ticks every 6 hour
 ax1.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M"))  # Format as month, day, and hour:minute
 
 # Show Legend
 ax1.legend(loc="upper left")
-
 plt.xticks(rotation= 45)
-
-# Set plot title
-plt.title("Humidity(%) Vs. Time (hourly)")
+plt.title("Humidity(%) Vs. Time (hourly)") # Set plot title
 
 # Display the plot
 plt.tight_layout()
