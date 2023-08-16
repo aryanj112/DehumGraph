4b761daf4a4410b458f58cd731ab3b90713e5d61
aryanj112 <2006aryanj@gmail.com>
2023-08-16 17:42:19 -0400

    Update hum_day.py

--- src/plot/ambient/hum_day.py
+++ src/plot/ambient/hum_day.py
@@ -16,8 +16,8 @@ ambient['Humidity(%)'] = pd.to_numeric(ambient['Humidity(%)'], errors='coerce')
 ambient.dropna(inplace=True)
 
 # Filter time
-start_date = pd.to_datetime("2023-07-05")
-end_date = pd.to_datetime("2023-07-06")
+start_date = pd.to_datetime("2023-07-04")
+end_date = pd.to_datetime("2023-07-05")
 ambient = ambient[ambient["Time"].between(start_date, end_date)]
 
 # Create a figure and plot
@@ -31,7 +31,7 @@ ax1.plot(x_interp, y_interp, '-', label="Humidity(%)", color="tab:purple")
 
 #ax1.plot(ambient["Time"], ambient["Humidity(%)"], '-', label="Humidity(%)", color="tab:purple")
 
-vertical_line_time = pd.to_datetime("2023-07-05 11:00")
+vertical_line_time = pd.to_datetime("2023-07-04 12:00")
 ax1.axvline(x=vertical_line_time, color='red', linestyle='--')
 
 ax1.set_xlabel("Date and Time")
