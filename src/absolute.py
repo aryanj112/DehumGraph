dfe19be27d0333b1f075593df81d711c60bb434d
aryanj112 <2006aryanj@gmail.com>
2023-08-18 13:10:06 -0400

    Update absolute.py
    Turned it into a function

--- src/absolute.py
+++ src/absolute.py
@@ -1,23 +1,20 @@
-# Initial variables
-temp = 20  # Measured in Fahrenheit
-relHum = 10  # Relative humidity
-Rw = 461.5   # Specific gas constant for water vapor (J/(kg·K))
+def relToAbs(temp,relHum):
+    # Initial variables
+    temp = temp  # Measured in Fahrenheit
+    relHum = relHum  # Relative humidity
+    Rw = 461.5   # Specific gas constant for water vapor (J/(kg·K))
 
-# Convert temperature from Fahrenheit to Celsius and Kelvin
-temp_celsius = (temp - 32) * 5 / 9
-temp_kelvin = temp_celsius + 273.15
-print(f"Temp in Celsius: {temp_celsius:.2f}")
-print(f"Temp in Kelvin: {temp_kelvin:.2f}")
+    # Convert temperature from Fahrenheit to Celsius and Kelvin
+    temp_celsius = (temp - 32) * 5 / 9
+    temp_kelvin = temp_celsius + 273.15
 
-# Calculate saturation vapor pressure (satuVap) using Clausius–Clapeyron equation
-satuVap = 6.11 * 10 ** ((7.5 * temp_celsius) / (237.3 + temp_celsius))  # Saturation vapor pressure in mb
+    # Calculate saturation vapor pressure (satuVap) using Clausius–Clapeyron equation
+    satuVap = 6.11 * 10 ** ((7.5 * temp_celsius) / (237.3 + temp_celsius))  # Saturation vapor pressure in mb
+    satuVap_pa = satuVap * 100 # Convert saturation vapor pressure from millibar to Pascal
 
-# Convert saturation vapor pressure from millibar to Pascal
-satuVap_pa = satuVap * 100
-print(f"Saturation Vapor Pressure: {satuVap_pa:.2f} Pa")
+    # Calculate absolute humidity (absHum)
+    absHum = (relHum * satuVap_pa) / (Rw * temp_kelvin * 100)
+    absHum_g = absHum * 1000 # Convert abs hum from kg/m^3 to g/m^3 
+    return absHum_g
 
-# Calculate absolute humidity (absHum)
-absHum = (relHum * satuVap_pa) / (Rw * temp_kelvin * 100)
-absHum_g = absHum * 1000
-print(f"Absolute Humidity: {absHum:.6f} kg/m^3")
-print(f"Absolute Humidity: {absHum_g:.6f} g/m^3")
+print(relToAbs(40,40))
