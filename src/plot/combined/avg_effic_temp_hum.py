bea810ff0ed78e53743e5a3c1af0b026a7497e23
aryanj112 <2006aryanj@gmail.com>
2023-08-21 17:28:28 -0400

    Update avg_effic_temp_hum.py
    Made this program input based and also added an option to average the data thus making the non averaging version obsolete

--- src/plot/combined/avg_effic_temp_hum.py
+++ src/plot/combined/avg_effic_temp_hum.py
@@ -1,65 +1,100 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
+import numpy as np
 from matplotlib.ticker import MaxNLocator
 
-# Read data
-dehum = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\dehumidifer_full_data.csv")
-weather = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\netzero\weather_file.csv")
-ambient = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\ambient_weather\ambient_CH1A_combine.csv")
+def plot_eff_tem_hum(year, loc, show, write, avg):
+    dehum = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\Dehumidifier_Full_Data_New.csv")
+    weather = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\netzero\weather_file.csv")
+    
+    dtype_options = {'Temperature(F)': float, 'Humidity(%)': float, 'Dewpoint(F)': float, 'HeatIndex(F)': float, 'Absolute Humidity(g/m^3)': float}
+    if loc == 'guestroom':
+        file_path = r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Base(guest)\CH7A_Absolute.csv"
+    elif loc == 'frontdoor':
+        file_path = r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Out_FR_door\CH1A_Absolute.csv"
+    else:
+        raise ValueError('Invalid location specified')
 
-# Convert data to datetime
-dehum["Start Date"] = pd.to_datetime(dehum["Start Date"])
-weather["date"] = pd.to_datetime(weather["date"])
-ambient["Time"] = pd.to_datetime(ambient["Time"])
+    # Read and preprocess ambient data
+    ambient = pd.read_csv(file_path, dtype=dtype_options, low_memory=False)
+    ambient.replace("'---.-'", np.nan, inplace=True)
+    ambient['Absolute Humidity(g/m^3)'] = pd.to_numeric(ambient['Absolute Humidity(g/m^3)'], errors='coerce')
+    ambient.dropna(inplace=True)
+    
+    dehum["Start Date"] = pd.to_datetime(dehum["Start Date"])
+    weather["date"] = pd.to_datetime(weather["date"])
+    ambient["Time"] = pd.to_datetime(ambient["Time"], format="%m/%d/%y %H:%M")
+    
+    # Filter data start and end time
+    start_date = pd.to_datetime(f"{year}-05-01")
+    end_date = pd.to_datetime(f"{year}-11-30") 
+    dehum = dehum[dehum["Start Date"].between(start_date, end_date)]
+    weather = weather[weather["date"].between(start_date, end_date)]
+    ambient = ambient[ambient["Time"].between(start_date, end_date)]
+    
+    ambient["Date"] = ambient["Time"].dt.date
+    daily_avg_humidity = ambient.groupby("Date")["Humidity(%)"].mean()
+    daily_avg_temp = weather.groupby(weather["date"].dt.date)["temperature"].mean()
 
-# Filter data start and end time
-start_date = pd.to_datetime("2021-01-01")
-end_date = pd.to_datetime("2023-08-08") 
-dehum = dehum[dehum["Start Date"].between(start_date, end_date)]
-weather = weather[weather["date"].between(start_date, end_date)]
-ambient = ambient[ambient["Time"].between(start_date, end_date)]
+    fig, ax1 = plt.subplots()
 
-# Calculate daily average humidity
-ambient["Date"] = ambient["Time"].dt.date
-daily_avg_humidity = ambient.groupby("Date")["Humidity(%)"].mean()
+    # Set the x-axis to show years
+    ax1.xaxis.set_major_locator(mdates.MonthLocator())
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter("%m"))
 
-# Calculate daily average temperature
-daily_avg_temp = weather.groupby(weather["date"].dt.date)["temperature"].mean()
+    ax1.set_ylim(0, 2)
+    ax1.plot(dehum["Start Date"], dehum["L/kWh"], '-', label="Efficiency [L/kWh]", color="tab:blue")
+    ax1.set_xlabel("Date")
+    ax1.set_ylabel("Efficiency [L/kWh]", color="tab:blue")
+    
+    ax2 = ax1.twinx()
+    ax3 = ax1.twinx()
 
-fig, ax1 = plt.subplots()
+    temp_label = 'Temperature [°F]'
+    hum_label = 'Absolute Humidity [g/m^3]'
 
-# Set the x-axis to show years
-ax1.xaxis.set_major_locator(mdates.YearLocator())
-ax1.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+    if avg == 'N':
+        ax2.plot(weather["date"],weather["temperature"], '-',label = temp_label, color = "tab:red")
+        ax3.plot(ambient["Time"], ambient["Absolute Humidity(g/m^3)"], '-', label= hum_label, color="tab:purple")
+    else:
+        temp_label = "Avg " + temp_label
+        hum_label = "Avg " + hum_label
+        ax2.plot(daily_avg_temp.index, daily_avg_temp.values, '-', label=temp_label, color="tab:red")
+        ax3.plot(daily_avg_humidity.index, daily_avg_humidity.values, '-', label= hum_label, color="tab:purple")
 
-# Plot first variable (efficiency) on the left y-axis
-ax1.plot(dehum["Start Date"], dehum["L/kWh"], '-', label="Efficiency", color="tab:blue")
-ax1.set_xlabel("Date")
-ax1.set_ylabel("Efficiency", color="tab:blue")
-#ax1.set_ylim(0, 2)
+    ax3.spines['right'].set_position(('outward', 80))
+    ax3.yaxis.set_major_locator(MaxNLocator(integer=True, nbins=15))
+    
+    ax2.set_ylabel(temp_label, color="tab:red")
+    ax3.set_ylabel(hum_label, color="tab:purple")
 
-# Create a second y-axis for temperature and humidity
-ax2 = ax1.twinx()
+    # Show Legend
+    lines, labels = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    lines3, labels3 = ax3.get_legend_handles_labels()
+    ax1.legend(lines + lines2+lines3, labels + labels2 + labels3, loc="upper left")
 
-# Plot daily average temperature on the right y-axis
-ax2.plot(daily_avg_temp.index, daily_avg_temp.values, '-', label="Avg Temperature", color="tab:red")
-ax2.set_ylabel("Avg Temperature (°C)", color="tab:red")
+    # Title and end
+    plt.title(f"Efficiency [L/kWh], {temp_label}, {hum_label} ({loc}) (May To November {year})")
+    fig.tight_layout()
+    
+    # Save the plot if requested
+    if write == 'Y':
+        plot_filename = fr'C:\Users\ajayj\DehumGraph\plots\Effic_Vs_AH ({loc}) (May_Nov_{year}).png'
+        plt.savefig(plot_filename, dpi=500, bbox_inches='tight')   
+        print(f"Plot saved as '{plot_filename}'")
+    
+    # Show the plot if requested
+    if show == 'Y':
+        print("Plot is being dsiplayed")
+        plt.show()
 
-# Create a third y-axis for average humidity
-ax3 = ax1.twinx()
-ax3.spines['right'].set_position(('outward', 80))
-ax3.yaxis.set_major_locator(MaxNLocator(integer=True, nbins=15))
-ax3.plot(daily_avg_humidity.index, daily_avg_humidity.values, '-', label="Avg Humidity (%)", color="tab:purple")
-ax3.set_ylabel("Avg Humidity (%)", color="tab:purple")
+if __name__ == '__main__':
+    year = input("Enter a year: ")
+    loc = input("Enter a location to plot Effic Vs. A.H. [guestroom or frontdoor]: ")
+    avg = input('Do you want to average the temperature and absolute humidity [Y/N]: ').upper()
+    show = input('Do you want to show this [Y/N]: ').upper() 
+    write = input('Do you want to save this [Y/N]: ').upper()
 
-# Show Legend
-lines, labels = ax1.get_legend_handles_labels()
-lines2, labels2 = ax2.get_legend_handles_labels()
-lines3, labels3 = ax3.get_legend_handles_labels()
-ax1.legend(lines + lines2+lines3, labels + labels2 + labels3, loc="upper left")
-
-# Title and end
-plt.title("Efficiency, Avg Humidity, and Avg Temperature (AFTER AVERAGING)")
-fig.tight_layout()
-plt.show()
+    plot_eff_tem_hum(year, loc, show, write, avg)
