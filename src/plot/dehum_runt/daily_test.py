a16d3e32e5969701e5d54ab65e40e41987561717
aryanj112 <2006aryanj@gmail.com>
2023-10-10 19:23:48 -0400

    New Exact_runt folder
    A folder dedicated to figuring out the exact time the dehumidifier was running and then averaging the absolute humidity for that day and then with that we will graph the ah vs eff graph
    
    Steps:
    Get the new aw data compiled
    Transcribe data
    Determain a time zone
    Get plots of the temperature of the outlet and that will tell us when the dehumidifier starts
    Then get the time the bucket was emptied and that is the end time
    Take these times and average the ah values for that day
    that value is now the ah for the day and the efficiency is already noted down
    plot each point

---
+++ src/plot/dehum_runt/daily_test.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+import numpy as np
+from scipy.interpolate import CubicSpline
+
+# Read data
+ambient = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Base(guest)\CH7A_Absolute.CSV")
+
+# Convert Time column to datetime
+ambient["Time"] = pd.to_datetime(ambient["Time"], format="%m/%d/%y %H:%M")
+
+# Clean data
+ambient.replace("--", np.nan, inplace=True)
+ambient['Absolute Humidity(g/m^3)'] = pd.to_numeric(ambient['Absolute Humidity(g/m^3)'], errors='coerce')
+ambient.dropna(inplace=True)
+
+# Filter time
+start_date = pd.to_datetime("2023-07-04")
+end_date = pd.to_datetime("2023-07-05")
+ambient = ambient[ambient["Time"].between(start_date, end_date)]
+
+# Create a figure and plot
+fig, ax1 = plt.subplots()
+
+#cubic_interp = CubicSpline(ambient["Time"], ambient["Absolute Humidity(%)"])
+#x_interp = pd.date_range(start=min(ambient["Time"]), end=max(ambient["Time"]), periods=100)
+#y_interp = cubic_interp(x_interp)
+
+#ax1.plot(x_interp, y_interp, '-', label="Absolute Humidity [g/m^3]", color="tab:purple")
+
+ax1.plot(ambient["Time"], ambient["Absolute Humidity(g/m^3)"], '-', label="Absolute Humidity [g/m^3]", color="tab:purple")
+#ax1.plot(ambient["Time"], ambient["Humidity(%)"], '-', label="Humidity(%)", color="tab:purple")
+
+vertical_line_time = pd.to_datetime("2023-07-04 12:00")
+ax1.axvline(x=vertical_line_time, color='red', linestyle='--')
+
+ax1.set_xlabel("Date and Time")
+ax1.set_ylabel("Absolute Humidity[g/m^3]", color="tab:purple")
+
+ax1.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+ax1.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M"))
+
+ax1.legend(loc="upper left")
+plt.xticks(rotation=45)
+plt.title("Absolute Humidity[g/m^3] Vs. Time (hourly)")
+
+plt.tight_layout()
+plt.show()
