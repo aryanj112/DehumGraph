d18e14e0809afc452eb0c8883a17f4981de26851
aryanj112 <2006aryanj@gmail.com>
2023-10-10 23:15:53 -0400

    changes

--- src/plot/dehum_runt/day_average.py
+++ src/plot/dehum_runt/day_average.py
@@ -1,13 +1,17 @@
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
 def day_average(input_date, new_date):    
-    
+    # Define the date and time format
+    print(input_date)
+    print(new_date)
+
+    date_time_format = "%m/%d/%y %H:%M"  # Assuming format is "month day year army time"
+
     # Read data
-    ambient = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Base(guest)\CH7A_Combine.csv")
+    ambient = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Base(guest)\CH7A_Absolute.csv")
 
     # Clean data
     ambient.replace("---.-", np.nan, inplace=True)
@@ -15,14 +19,15 @@ def day_average(input_date, new_date):
     ambient.dropna(inplace=True)
 
     # Filter time
-    ambient["Time"] = pd.to_datetime(ambient["Time"], format="%m/%d/%y %H:%M")
+    ambient["Time"] = pd.to_datetime(ambient["Time"], format=date_time_format)  # Use the specified format
     ambient = ambient[ambient["Time"].between(input_date, new_date)]
 
     day_avg_temperature = ambient["Temperature(F)"].mean()
 
+    print(day_avg_temperature)
     return day_avg_temperature
 
 if __name__ == '__main__':
-    input_date = input("input date:")
-    new_date = input("new_date: ")
+    input_date = input("Enter start date (e.g., 04/05/2023 14:49): ")
+    new_date = input("Enter end date (e.g., 04/06/2023 14:49): ")
     print(day_average(input_date, new_date))
--- src/plot/dehum_runt/dehum_start_end.py
+++ src/plot/dehum_runt/dehum_start_end.py
@@ -5,40 +5,53 @@ import numpy as np
 from datetime import datetime, timedelta
 import day_average as da
 
-def dehum_start_end(month, day):    
-    
+def dehum_start_end(month, day):
+
     # Read data
-    ambient = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Deh(out)\2023CH6A_use.csv")
+    temperature_data = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Deh(out)\2023CH6A_use.csv")
 
     # Clean data
-    ambient.replace("---.-", np.nan, inplace=True)
-    ambient["Temperature(F)"] = pd.to_numeric(ambient["Temperature(F)"], errors='coerce')
-    ambient.dropna(inplace=True)
+    temperature_data.replace("---.-", np.nan, inplace=True)
+    temperature_data["Temperature(F)"] = pd.to_numeric(temperature_data["Temperature(F)"], errors='coerce')
+    temperature_data.dropna(inplace=True)
 
     # Filter time
-    ambient["Time"] = pd.to_datetime(ambient["Time"], format="%m/%d/%y %H:%M")
-    month = int(month)
-    day = int(day)
-    input_date = datetime(year=2023, month=month, day=day)
-    new_date = input_date + timedelta(days=1)
-    ambient = ambient[ambient["Time"].between(input_date, new_date)]
 
+    year = 2023  
+    month = int(month)  
+    day = int(day)  
+    hour = 0
+    minute = 0
+    input_date = datetime(year, month, day, hour, minute)
+    #input_date = input_date.strftime("%m/%d/%y %H:%M")
+    new_date = input_date + timedelta(days=1)
+    input_date = pd.to_datetime(input_date, format= "%m/%d/%y %H:%M")
+    new_date = pd.to_datetime(new_date, format= "%m/%d/%y %H:%M")
+
+
+    temperature_data["Time"] = pd.to_datetime(temperature_data["Time"], format= "%m/%d/%y %H:%M")
+    temperature_data = temperature_data[temperature_data["Time"].between(input_date, new_date)]
+
+    # Get the daily average temperature using the 'day_average' module
+    print('error')
     day_avg_temperature = da.day_average(input_date, new_date)
-    print (day_avg_temperature)
-    
+
     # Plot
     fig, ax1 = plt.subplots()
-    ax1.plot(ambient["Time"], ambient["Temperature(F)"], '-', label="Temperature(F)", color="tab:red")
+    ax1.plot(temperature_data["Time"], temperature_data["Temperature(F)"], '-', label="Temperature(F)", color="tab:red")
     vertical_line_time = pd.to_datetime("2023-07-18 12:00")
     ax1.axvline(x=vertical_line_time, color='red', linestyle='--')
     ax1.set_xlabel("Time")
     ax1.set_ylabel("Temperature(F)", color="tab:red")
     ax1.xaxis.set_major_locator(mdates.HourLocator(interval=2))
     ax1.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M"))
-    
+
     # Plot the daily average temperature as a horizontal line
-    #ax1.axhline(y=day_avg_temperature.mean(), color='tab:blue', linestyle='--', label="Daily Avg Temperature")
-    
+    print("get")
+    print(day_avg_temperature)
+    if not pd.isna(day_avg_temperature):
+        ax1.axhline(y=day_avg_temperature, color='tab:blue', linestyle='--', label="Daily Avg Temperature")
+
     ax1.legend(loc="upper left")
     plt.xticks(rotation=45)
     plt.title("Temperature(F) Vs. Time (H)")
