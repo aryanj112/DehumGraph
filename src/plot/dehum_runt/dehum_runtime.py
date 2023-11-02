2e9d1d151c62e9cd8168e34ea126f2057a686331
aryanj112 <2006aryanj@gmail.com>
2023-11-02 09:21:32 -0400

    Update dehum_runtime.py
    Fixed issues with addition by filling in NaN values

--- src/plot/dehum_runt/dehum_runtime.py
+++ src/plot/dehum_runt/dehum_runtime.py
@@ -10,22 +10,27 @@ dehum_run = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\dehum_runtimes_update.c
 def avg_time(write):
     dehum_run['Date'] = pd.to_datetime(dehum_run['Date'], format='%m/%d/%Y')
 
-    dehum_run['Dehumidifier Time'] = dehum_run['Run Zone 1'].apply(lambda x: int(x.split('-')[1]) - int(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan)
-
-    daily_dehumidifier_time = dehum_run.groupby('Date')['Dehumidifier Time'].sum()
+    dehum_run['Dehumidifier Time 1'] = dehum_run['Run Zone 1'].apply(lambda x: int(x.split('-')[1]) - int(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan)
+    dehum_run['Dehumidifier Time 2'] = dehum_run['Run Zone 2'].apply(lambda x: int(x.split('-')[1]) - int(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan)
+    dehum_run['Dehumidifier Time 1'] = dehum_run['Dehumidifier Time 1'].fillna(0)
+    dehum_run['Dehumidifier Time 2'] = dehum_run['Dehumidifier Time 2'].fillna(0)
+    dehum_run['Final'] = dehum_run['Dehumidifier Time 1'] + dehum_run['Dehumidifier Time 2']
 
     plt.figure(figsize=(12, 6))
-    plt.plot(daily_dehumidifier_time.index, daily_dehumidifier_time, marker='o', linestyle='-', color='b')
+    plt.plot(dehum_run['Date'], dehum_run['Final'], marker='o', linestyle='-', color='b')
     plt.xlabel('Date')
     plt.ylabel('Added Dehumidifier Time (hours)')
-    plt.title('Added Dehumidifier Time Each Day')
+    plt.title('Daily Dehumidifier Run Time')
     plt.grid(True)
     plt.xticks(rotation=45)
     plt.tight_layout()
-    
+    plt.ylim(-1, 25)
+
     if (write == 'Y'):
-        plt.savefig("Dehumidifier Run time (Hours)", dpi=500, bbox_inches='tight')   
-    
+        plot_filename = fr'C:\Users\ajayj\DehumGraph\plots\others\Daily_Dehum_Run_Time.png'
+        plt.savefig(plot_filename, dpi=500, bbox_inches='tight')   
+        print(f"Plot saved as '{plot_filename}'")
+
     plt.show()
 
 if __name__ == '__main__':
