7ec376c0a912b4d0f03e0df31b54ad1053ca9c96
aryanj112 <2006aryanj@gmail.com>
2023-11-04 11:00:13 -0400

    Update abs_hum_day_cal.py
    will change this all for a chatgpt version so i am keeping here for safety

--- src/plot/dehum_runt/abs_hum_day_cal.py
+++ src/plot/dehum_runt/abs_hum_day_cal.py
@@ -1,52 +1,33 @@
-# Calculate Daily Absolute Humidity based on Dehum Runtimes
-
-# For each day:
-#   Get the time the dehumidifier is running
-#   Get the absolute humidity for that time
-#   Put the absolute humidiity as a meaned value and save it in a data set
-
-import matplotlib as mlt
 import pandas as pd
 import numpy as np
 
 ambient = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Base(guest)\CH7A_Absolute.csv")
+ambient["Time"] = pd.to_datetime(ambient["Time"], format="%m/%d/%y %H:%M")
 run_times = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\dehum_runtimes_update.csv")
-
-'''
-start_date = pd.to_datetime(f'2023-06-18')
-end_date = pd.to_datetime(f'2023-8-14')
-ambient = ambient[ambient["Time"].between(start_date, end_date)]
-'''
 
 run_times['RT1.1'] = run_times['Run Zone 1'].apply(lambda x: int(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
 run_times['RT1.2'] = run_times['Run Zone 1'].apply(lambda x: int(x.split('-')[1]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
 run_times['RT2.1'] = run_times['Run Zone 2'].apply(lambda x: int(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
 run_times['RT2.2'] = run_times['Run Zone 2'].apply(lambda x: int(x.split('-')[1]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
 
-print(run_times)
+start_time = pd.to_datetime('2023-06-18')
+end_time = pd.to_datetime('2023-08-15')
 
-#abs_hum_avr_test = run_times[]
+run_times['Date'] = pd.to_datetime(run_times['Date'], format='%m/%d/%y %H:%M')
+run_times['RT1.1'] = run_times['Date'] + pd.to_timedelta(run_times['RT1.1'], unit='h')
+run_times['RT1.2'] = run_times['Date'] + pd.to_timedelta(run_times['RT1.2'], unit='h')
 
-'''
-run_times['Average Absolute Humidity'] = 
+def calculate_mean(row):
+    selected_data = ambient[
+        (ambient['Time'] >= row['RT1.1']) &
+        (ambient['Time'] <= row['RT1.2'])
+    ]
+    if not selected_data.empty:
+        return selected_data['Absolute Humidity(g/m^3)'].mean()
+    return np.nan
 
-Assuming the 'Time Range' column in the 'time_ranges' DataFrame contains entries like "8-12"
-time_ranges['Start Time'] = time_ranges['Time Range'].apply(lambda x: datetime.strptime(x.split('-')[0], '%H:%M'))
-time_ranges['End Time'] = time_ranges['Time Range'].apply(lambda x: datetime.strptime(x.split('-')[1], '%H:%M'))
+run_times['Average Absolute Humidity'] = run_times.apply(calculate_mean, axis=1)
 
-
-averages = []
-for _, row in run_times.iterrows():
-    start_time = row['Start Time']
-    end_time = row['End Time']
-    mask = (data['Timestamp'] >= start_time) & (data['Timestamp'] <= end_time)
-    average_time = data.loc[mask, 'Value'].mean()
-    averages.append(average_time)
-
-time_ranges['Average Value'] = averages
-
-
-for x in ambient:
-    print(x)
-
-'''
+mean_absolute_humidity = ambient[(ambient['Time'] >= start_time) & (ambient['Time'] <= end_time)]['Absolute Humidity(g/m^3)']
+grouped = mean_absolute_humidity.groupby(ambient['Time']).mean()
+print(grouped)
