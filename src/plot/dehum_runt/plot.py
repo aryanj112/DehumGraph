517854f9172a75c43a08b2254df245a3de695498
aryanj112 <2006aryanj@gmail.com>
2023-11-06 15:13:31 -0500

    Update plot.py
    FIXED and workng
    
    Get more data and then remake this

--- src/plot/dehum_runt/plot.py
+++ src/plot/dehum_runt/plot.py
@@ -1,24 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-run_time = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\dehum_rt_avg_AH_new.csv")
-dehum = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\Dehumidifier_Full_Data_New_Form.csv")    
+run_time = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\dehum_rt_avg_AH_new_USE - dehum_rt_avg_AH_new_USE.csv.csv")
+dehum = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\Dehumidifier_Full_Data_New - dehumidifer_full_data.csv")    
 
-run_time["Date_Only"] = pd.to_datetime(run_time["Date_Only"])
+run_time["Date"] = pd.to_datetime(run_time["Date"], format="%m/%d/%y")
 dehum["Start Date"] = pd.to_datetime(dehum["Start Date"])
 
-start_date = pd.to_datetime('2023-06-14')
+'''
+start_date = pd.to_datetime('2022-06-10')
 end_date = pd.to_datetime('2023-8-18') 
 dehum = dehum[dehum["Start Date"].between(start_date, end_date)]
-dehum_grouped = dehum.groupby("Start Date")["L/kWh"].mean()
-runtime_grouped = run_time.groupby("Date_Only")["Average Absolute Humidity Zone 1"].mean()
+'''
 
-print(dehum_grouped)
-print(runtime_grouped)
+runtime_grouped = run_time.groupby(run_time["Date"].dt.date)["Average Absolute Humidity Zone 1"].mean()
+dehum_grouped = dehum.groupby("Start Date")["L/kWh"].mean()
+merged_data = pd.merge(dehum_grouped, runtime_grouped, left_index=True, right_index=True, how="left")
 
 plt.figure()
-#plt.plot(runtime_grouped.index, dehum_grouped, 'o', label="Efficiency [L/kWh]", color="tab:orange")66
-#plt.plot(runtime_grouped["Average Absolute Humidity Zone 1"], dehum_grouped["L/kWh"], 'o', label="Efficiency [L/kWh]", color="tab:orange")
+plt.plot(merged_data["Average Absolute Humidity Zone 1"], merged_data["L/kWh"], 'o', label="Efficiency [L/kWh]", color="tab:blue")
 plt.xlabel("Run Time Absolute Humidity (g/m^3)")
 plt.ylabel("Efficiency [L/kWh]")
 plt.title(f"Efficiency [L/kWh] Vs. Dehum. Running Absolute Humidity (g/m^3) (guestroom)")
