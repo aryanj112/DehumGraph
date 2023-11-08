2686e76f27cf7f324ff421db80a2ed68c947e5eb
aryanj112 <2006aryanj@gmail.com>
2023-11-08 13:50:52 -0500

    way too much

--- src/plot/dehum_runt/abs_hum_day_cal.py
+++ src/plot/dehum_runt/abs_hum_day_cal.py
@@ -2,17 +2,22 @@
 
 import pandas as pd
 import numpy as np
+import re
 
 ambient = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\ambient_weather\Base(guest)\CH7A_Absolute_NEW.csv")
 ambient["Time"] = pd.to_datetime(ambient["Time"], format="%m/%d/%y %H:%M")
-run_times = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\dehum_runtimes_update.csv")
+#run_times = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\dehum_runtimes_update.csv")
+run_times = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\runtimes\fin_dehum_runtimes.csv")
 
-run_times['RT1.1'] = run_times['Run Zone 1'].apply(lambda x: int(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
-run_times['RT1.2'] = run_times['Run Zone 1'].apply(lambda x: int(x.split('-')[1]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
-run_times['RT2.1'] = run_times['Run Zone 2'].apply(lambda x: int(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
-run_times['RT2.2'] = run_times['Run Zone 2'].apply(lambda x: int(x.split('-')[1]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
+run_times['RT1.1'] = run_times['Run Zone 1'].apply(lambda x: float(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan).fillna(0.0)
+run_times['RT1.2'] = run_times['Run Zone 1'].apply(lambda x: float(x.split('-')[1]) if isinstance(x, str) and '-' in x else np.nan).fillna(0.0)
 
-run_times['Date'] = pd.to_datetime(run_times['Date'], format='%m/%d/%y %H:%M')
+#run_times['RT1.1'] = run_times['Run Zone 1'].apply(lambda x: int(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
+#run_times['RT1.2'] = run_times['Run Zone 1'].apply(lambda x: int(x.split('-')[1]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
+run_times['RT2.1'] = run_times['Run Zone 2'].apply(lambda x: float(x.split('-')[0]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
+run_times['RT2.2'] = run_times['Run Zone 2'].apply(lambda x: float(x.split('-')[1]) if isinstance(x, str) and '-' in x else np.nan).fillna(0)
+
+run_times['Date'] = pd.to_datetime(run_times['Date'], format='%m/%d/%y')
 run_times['RT1.1'] = run_times['Date'] + pd.to_timedelta(run_times['RT1.1'], unit='h')
 run_times['RT1.2'] = run_times['Date'] + pd.to_timedelta(run_times['RT1.2'], unit='h')
 run_times['RT2.1'] = run_times['Date'] + pd.to_timedelta(run_times['RT2.1'], unit='h')
--- src/plot/dehum_runt/plot.py
+++ src/plot/dehum_runt/plot.py
@@ -1,17 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-run_time = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\dehum_rt_avg_AH_new_USE - dehum_rt_avg_AH_new_USE.csv.csv")
-dehum = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\Dehumidifier_Full_Data_New - dehumidifer_full_data.csv")    
+run_time = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\runtimes\dehum_rt_avg_AH_new.csv")
+dehum = pd.read_csv(r"C:\Users\ajayj\DehumGraph\data\efficiency\Dehumidifier_Full_Data_New - dehumidifer_full_data.csv")    
 
-run_time["Date"] = pd.to_datetime(run_time["Date"], format="%m/%d/%y")
+run_time["Date"] = pd.to_datetime(run_time["Date"], format="%Y-%m-%d")
 dehum["Start Date"] = pd.to_datetime(dehum["Start Date"])
 
-'''
-start_date = pd.to_datetime('2022-06-10')
-end_date = pd.to_datetime('2023-8-18') 
+start_date = pd.to_datetime('2022-10-14')
+end_date = pd.to_datetime('2023-10-30') 
 dehum = dehum[dehum["Start Date"].between(start_date, end_date)]
-'''
 
 runtime_grouped = run_time.groupby(run_time["Date"].dt.date)["Average Absolute Humidity Zone 1"].mean()
 dehum_grouped = dehum.groupby("Start Date")["L/kWh"].mean()
