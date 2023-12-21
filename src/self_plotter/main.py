59a6489c15875fe2acad9185bdeb454c2df2badb
aryanj112 <2006aryanj@gmail.com>
2023-12-21 10:32:35 -0500

    Update main.py

--- src/self_plotter/main.py
+++ src/self_plotter/main.py
@@ -8,17 +8,14 @@ dehumdidifier = r"C:\Users\ajayj\DehumGraph\data\Main Data\Dehum.csv"
 
 x_label = "Time"
 y_label = "Humidity(%)"
-y2_label = "Temperature(F)"
 
 x,y,data_set = data_loader(x_label, y_label,dehum_input)
-
-title = y_label + " & " + y2_label + " vs. " + x_label
-
+title = y_label + " vs. " + x_label
 fig, ax1 = basic_plot(x, y, x_label, y_label, title)
 
-x,y,data_set = data_loader(x_label, y2_label,dehum_input)
+#y2_label = "Temperature(F)"
+#x,y,data_set = data_loader(x_label, y2_label,dehum_input)
+#add_secondary_axis(fig, ax1, x, y, y2_label)
+#save_plot(title)
 
-add_secondary_axis(fig, ax1, x, y, y2_label)
-
-save_plot(title)
 show_plot()
