5c89aae9a8d7c26132fe427bfc6d6c8a8c4183c7
aryanj112 <2006aryanj@gmail.com>
2023-12-21 09:56:40 -0500

    main and plotter changes
    small adjustments to main to avoid repetition of titles
    
    plotter has show_plot() so now we dnot have to have showplot in basic_plug

--- src/self_plotter/main.py
+++ src/self_plotter/main.py
@@ -1,7 +1,18 @@
-from data_loader import data_loader as data_loader
+from data_loader import data_loader
+from plotter import basic_plot, show_plot
 
+basement_guest = r"C:\Users\ajayj\DehumGraph\data\Main Data\Base(guest).csv"
 dehum_input = r"C:\Users\ajayj\DehumGraph\data\Main Data\Deh(in).csv"
+dehum_output = r"C:\Users\ajayj\DehumGraph\data\Main Data\Deh(out).csv"
+dehumdidifier = r"C:\Users\ajayj\DehumGraph\data\Main Data\Dehum.csv"
 
-data = data_loader(dehum_input)
-print(data.head())
-print(data.shape)
+x_label = "Temperature(F)"
+y_label = "Humidity(%)"
+
+x,y,data_set = data_loader(x_label, y_label,dehum_input)
+
+title = x_label + " vs. " + y_label
+
+basic_plot(x, y, x_label, y_label, title)
+
+show_plot()
--- src/self_plotter/plotter.py
+++ src/self_plotter/plotter.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 
 def basic_plot(x, y, x_label, y_label, title):
     fig, ax1 = plt.subplots()
-    ax1.plot(x,y,"-", label = title, color="tab:blue")
+    ax1.plot(x,y,"o", label = y_label, color="tab:blue")
     ax1.set_xlabel(x_label)
     ax1.set_ylabel(y_label)
     ax1.set_title(title)
     ax1.legend()
-    
     return fig, ax1
 
 def add_vertical_line():
@@ -26,4 +25,5 @@ def save_plot():
     return 0
 
 def show_plot():
-    return 0 
+    plt.tight_layout()
+    plt.show()
