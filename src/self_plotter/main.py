d0a7633a39d8a68f4c179949dc08e401ab0b5a02
aryanj112 <2006aryanj@gmail.com>
2023-12-21 10:20:55 -0500

    Created plotter.py
    Made a function that can save the files

--- src/self_plotter/main.py
+++ src/self_plotter/main.py
@@ -1,18 +1,25 @@
 from data_loader import data_loader
-from plotter import basic_plot, show_plot
+from plotter import basic_plot, show_plot, add_secondary_axis, save_plot
 
 basement_guest = r"C:\Users\ajayj\DehumGraph\data\Main Data\Base(guest).csv"
 dehum_input = r"C:\Users\ajayj\DehumGraph\data\Main Data\Deh(in).csv"
 dehum_output = r"C:\Users\ajayj\DehumGraph\data\Main Data\Deh(out).csv"
 dehumdidifier = r"C:\Users\ajayj\DehumGraph\data\Main Data\Dehum.csv"
 
-x_label = "Temperature(F)"
+x_label = "Time"
 y_label = "Humidity(%)"
+y2_label = "Temperature(F)"
 
 x,y,data_set = data_loader(x_label, y_label,dehum_input)
 
-title = x_label + " vs. " + y_label
+title = y_label + " & " + y2_label + " vs. " + x_label
 
-basic_plot(x, y, x_label, y_label, title)
+fig, ax1 = basic_plot(x, y, x_label, y_label, title)
 
+
+x,y,data_set = data_loader(x_label, y2_label,dehum_input)
+
+add_secondary_axis(fig, ax1, x, y, y2_label)
+
+save_plot(title)
 show_plot()
--- src/self_plotter/plotter.py
+++ src/self_plotter/plotter.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 def basic_plot(x, y, x_label, y_label, title):
     fig, ax1 = plt.subplots()
-    ax1.plot(x,y,"o", label = y_label, color="tab:blue")
+    ax1.plot(x,y,"-", label = y_label, color="tab:blue")
     ax1.set_xlabel(x_label)
     ax1.set_ylabel(y_label)
     ax1.set_title(title)
@@ -15,14 +15,16 @@ def add_vertical_line():
 
 def add_secondary_axis(fig, ax1, x, y, y_label):
     ax2 = ax1.twinx()
-    ax2.plot(x,y,"o", label = y_label, color="tab:purple")
+    ax2.plot(x,y,"-", label = y_label, color="tab:purple")
     return fig, ax1
 
 def add_tertiary_axis():
     return 0
 
-def save_plot():
-
+def save_plot(title):
+    plot_filename = fr"C:\Users\ajayj\DehumGraph\plots\plotter\{title}"
+    plt.savefig(plot_filename, dpi=500, bbox_inches='tight')
+    print(f"Plot saved as '{plot_filename}'")
     return 0
 
 def show_plot():
